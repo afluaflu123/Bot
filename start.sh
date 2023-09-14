9a75b4b1c965db9ad620b9540ebfd713f88a274e
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-09-14 22:59:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/afluaflu123/Eva-bot.git /Eva-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /
+  git clone $UPSTREAM_REPO /Eva-bot
 fi
 cd /Eva-bot
 pip3 install -U -r requirements.txt
